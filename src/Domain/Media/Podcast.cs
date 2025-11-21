918453bf9b5858d861271d34750a776514db80dc
Paul Epure <paepu25@student.sdu.dk>
2025-11-21 13:40:03 +0100

    finished podcast

---
+++ src/Domain/Media/Podcast.cs
@@ -0,0 +1,22 @@
+using workshop.Domain.Interfaces;
+
+namespace workshop.Domain.Media
+{
+	public class Podcast
+	{
+		public string[] Host;
+		public string[] Guest;
+		public int EpisodeNumber;
+		public string Language;
+
+		public void Listen()
+		{
+
+		}
+
+		public void Complete()
+		{
+
+		}
+	}
+}
